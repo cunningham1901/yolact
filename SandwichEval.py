b4de11a054202acada282a593dc301a26cb01d2a
joe <joe.20000@hotmail.co.uk>
2022-03-01 12:45:25 +0000

    Get network output for sandwich evaluation

---
+++ SandwichEval.py
@@ -0,0 +1,30 @@
+"""
+
+1. Run model over frame
+2. Identify sandwiches
+3. Make measurements and output
+
+"""
+from yolact import Yolact
+from data import cfg, set_cfg
+
+def evalframe(frame):
+    batch = FastBaseTransform()(frame.unsqueeze(0))
+    preds = net(batch)
+
+    return preds
+
+if __name__ == "__main__":
+    # Set up network with sandwich defaults
+    set_cfg('yolact_plus_resnet50_config')  # Set config
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')  # Setup pytorch cuda
+    dataset = None  # Set dataset none
+
+    net = Yolact()
+    net.load_weights('weights/yolact_plus_resnet50_112_28800.pth')
+    net.eval()  # set to evaluation mode
+    net.cuda()  # use cuda
+
+    path = '../images_00/0001.png'
+    frame = torch.from_numpy(cv2.imread(path)).cuda().float()
+    print(evalframe(frame))
